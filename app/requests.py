2f2056b477255b4db266f6e44a088f595c287012
gabrielcoder <gabrielcoder247@gmail.com>
2018-08-27 21:48:35 +0300

    create configure request function

---
+++ app/requests.py
@@ -0,0 +1,18 @@
+import urllib.request,json
+from .models import Source,Article
+from . import main
+
+# Getting Api Key
+api_Key = None
+#Getting the base urls
+sources_base_url = None
+articles_base_url = None
+
+def configure_request(app):
+	'''
+	Function to acquire the api key and base urls
+	'''
+	global api_Key,sources_base_url,articles_base_url
+	api_Key = app.config['NEWS_API_KEY']
+	sources_base_url = app.config['NEWS_SOURCES_BASE_URL']
+	articles_base_url = app.config['NEWS_ARTICLES_BASE_URL']
