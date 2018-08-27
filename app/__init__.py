e7097ef573d0f481ddb017c748f317a1116906c8
gabrielcoder <gabrielcoder247@gmail.com>
2018-08-27 21:18:28 +0300

    create sources class

--- app/__init__.py
+++ app/__init__.py
@@ -16,10 +16,10 @@ def create_app(config_name):
 	# initialize flask extensions
 	bootstrap.init_app(app)
 
-	# # registering the blueprint
-	# from .main import main as main_blueprint
+	# registering the blueprint
+	from .main import main as main_blueprint
 
-	# app.register_blueprint(main_blueprint)
+	app.register_blueprint(main_blueprint)
 
 	# setting config 
 	# from .request import configure_request
