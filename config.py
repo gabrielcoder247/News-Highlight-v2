c7a2c2661c3a73cf34cadb083d67a5e537771625
gabrielcoder <gabrielcoder247@gmail.com>
2018-08-28 12:16:07 +0300

    create api key

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
     NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v1/sources?language=en&category={}'
     NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v1/articles?source={}&apiKey={}'
-    
+     NEWS_API_KEY = '5c5003d54173471dac0e4ebad5d67923'
 
 class ProdConfig(Config):
     '''
