106bd8fc25b24a47314d1f143d78fc76c5ab1123
gabrielcoder <gabrielcoder247@gmail.com>
2018-08-28 12:47:38 +0300

    create secret-key

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v1/sources?language=en&category={}'
     NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v1/articles?source={}&apiKey={}'
     NEWS_API_KEY = '5c5003d54173471dac0e4ebad5d67923'
+    SECRET_KEY = '12345'
 
 class ProdConfig(Config):
     '''
